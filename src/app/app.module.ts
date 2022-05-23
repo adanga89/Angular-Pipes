7b667440c592fc5bafa16ad22b2d96d59af72848
Adan Garnica A <cuadrado015@gmail.com>
2022-05-23 16:09:31 -0500

    String y Date | Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el idioma local de la app
+import localES from '@angular/common/locales/es-MX';
+import localFR from '@angular/common/locales/fr';
+import localDE from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localES);
+registerLocaleData(localFR);
+registerLocaleData(localDE);
 
 @NgModule({
   declarations: [
@@ -13,10 +24,15 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    VentasModule,
     SharedModule,
-    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-MX'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
